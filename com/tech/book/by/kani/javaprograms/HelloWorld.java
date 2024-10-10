7e146c6c12107a07b2492d744c6c31df64878181
kanim <techbookbykani@gmail.com>
2024-10-11 00:06:52 +0530

    Committed first HelloWorld Program.

---
+++ com/tech/book/by/kani/javaprograms/HelloWorld.java
@@ -0,0 +1,6 @@
+package com.tech.book.by.kani.javaprograms;
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World by techbookbykani");
+    }
+}
